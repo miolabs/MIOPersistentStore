dc429fb0565d6f3e7ff26a454b31c3f3519afff7
Javier Segura <javier@miolabs.com>
2025-02-07 12:01:22 +0100

    adding mio core dependency

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/miolabs/MIOCoreData.git", branch: "master" ),
+        .package(url: "https://github.com/miolabs/MIOCore.git", branch: "master" ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,6 +28,7 @@ let package = Package(
             dependencies: [
                 .product(name: "MIOCoreData", package: "MIOCoreData"),
                 .product(name: "CoreDataSwift", package: "MIOCoreData"),
+                .product(name: "MIOCoreLogger", package: "MIOCore"),
             ]),
         .testTarget(
             name: "MIOPersistentStoreTests",
